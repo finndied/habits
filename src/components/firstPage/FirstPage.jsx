87a1aa4fadfd1020f37edd499543c105822ada92
Ilya Morozov <finnlivefx1@mail.ru>
2023-03-31 23:37:31 +0700

    #4

--- src/components/firstPage/FirstPage.jsx
+++ src/components/firstPage/FirstPage.jsx
@@ -1,18 +1,22 @@
-import React from "react";
-import MyButton from "../UI/MyButton";
+import React from 'react'
+import MyButton from '../UI/MyButton'
 import cl from './FirstPage.module.css'
 
 const FirstPage = () => {
-  return (
-    <div>
-      <div className={cl.wrapper}>
-        <img src="src/components/firstPage/humanFirstPage.png" alt="" width='300px'></img>
-        <h1>Organize your life with Habito</h1>
-        <p>Habito help you build new habits based on behavior science</p>
-      </div>
-      <MyButton to="/MainPage">Got it!</MyButton>
-    </div>
-  );
-};
+	return (
+		<div>
+			<div className={cl.wrapper}>
+				<img
+					src="public/humanFirstPage.png"
+					alt=''
+					width='300px'
+				></img>
+				<h1>Organize your life with Habito</h1>
+				<p>Habito help you build new habits based on behavior science</p>
+			</div>
+			<MyButton to='/MainPage'>Got it!</MyButton>
+		</div>
+	)
+}
 
-export default FirstPage;
+export default FirstPage
